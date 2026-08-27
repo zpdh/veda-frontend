d140a1a0f0b670b48131d4dfdd5407bcdf4371df
Antunes <36316038+zpdh@users.noreply.github.com>
2026-08-27 00:21:56 -0300

    chore: add Dockerfile and nginx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AppLayout } from "./core/components/AppLayout";
 import { LeaderboardPage } from "./features/leaderboard/pages/LeaderboardPage";
 import { HomePage } from "./core/pages/HomePage";
