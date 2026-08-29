7103c8f19f07f7f52c160fbec72eb2bd949f3c3f
Antunes <36316038+zpdh@users.noreply.github.com>
2026-08-29 19:09:40 -0300

    feat: add title, icon and vercel config

--- src/core/components/Footer.tsx
+++ src/core/components/Footer.tsx
@@ -1,3 +1,4 @@
+import iconsUrl from "../../assets/icons.svg"
 export function Footer() {
   return (
     <footer className="flex items-center justify-center gap-5 border-t border-veda-border py-8">
@@ -6,8 +7,11 @@ export function Footer() {
           href="https://github.com/zpdh/monumenta-veda"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-xs text-veda-text-muted hover:text-veda-text"
+          className="text-base text-veda-text-muted hover:text-veda-text flex items-center gap-1"
         >
+          <svg className="h-3.5 w-3.5 fill-current" viewBox="0 0 19 19">
+            <use href={`${iconsUrl}#github-icon`}/>
+          </svg>
           Repository
         </a>
       </span>
